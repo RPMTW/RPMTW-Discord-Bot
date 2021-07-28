eba3ccefa02a031036181b65c28300abe525b46a
菘菘 <rrt467778@gmail.com>
2021-07-28 19:14:20 +0800

    垃圾訊息刪除功能

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,16 +1,14 @@
 module.exports = async (reaction, user, client) => {
+    if (user.bot) return;
     if (reaction.partial) {
         try {
             await reaction.fetch();
         } catch (error) {
-            console.error('Fetching message failed: ', error);
+            console.error('取得資訊失敗: ', error);
             return;
         }
     }
-    if (!user.bot) {
-        console.log(reaction.emoji.id);
-        if (reaction.emoji.name == "♻️") { 
-            console.log("Reaction added: " + reaction.emoji.name);
-        }
+    if (reaction.emoji.name == "♻️" && reaction.emoji.count >= 5 && !reaction.message.deleted) {
+        await reaction.message.delete();
     }
 }
