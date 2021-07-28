a8300f8f4e3b3887ab57c58031dff2422671de1b
菘菘 <rrt467778@gmail.com>
2021-07-28 18:13:40 +0800

    新增編編輯訊息紀錄

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,13 +1,11 @@
 module.exports = async (oldMessage, newMessage, client) => {
-            let embed = new MessageEmbed()
-                .setTitle(`PRMTW 資訊`)
-                .setDescription("這個分頁會顯示關於此資源包的相關翻譯資訊")
-                .addField("翻譯進度", translationProgress, true)
-                .addField("已核准翻譯進度", approvalProgress, true)
-                .addField("翻譯字數", words_translated + "/" + words_total + "個文字")
-                .addField("翻譯字串", phrases_translated + "/" + phrases_total + "個字串")
-                .setFooter(`由${msg.author.tag}查詢的資料`, `${msg.author.displayAvatarURL({ dynamic: true })}`)
-                .setTimestamp()
-                .setColor("DARK_BLUE");
-            msg.channel.send(embed);
+    let chennel = client.channels.fetch("832849374395760660");
+    let embed = new MessageEmbed()
+        .setTitle(`訊息編輯紀錄`)
+        .setDescription(`使用者: ${oldMessage.author} 在 ${oldMessage.channel} 編輯訊息`)
+        .addField("原始訊息", oldMessage.content, true)
+        .addField("修改後訊息", newMessage.content, true)
+        .setTimestamp()
+        .setColor("GREEN");
+    chennel.send(embed);
 };
