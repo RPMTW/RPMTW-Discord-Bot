e7c5100162e70a6af5aa80578ce3380fca3de514
菘菘 <rrt467778@gmail.com>
2021-07-28 18:19:23 +0800

    fix some bug

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = async (oldMessage, newMessage, client) => {
-    let chennel = client.channels.fetch("832849374395760660");
+    let chennel = client.channels.cache.find(channel => channel.id === `832849374395760660`);;
     let embed = new MessageEmbed()
         .setTitle(`訊息編輯紀錄`)
         .setDescription(`使用者: ${oldMessage.author} 在 ${oldMessage.channel} 編輯訊息`)
