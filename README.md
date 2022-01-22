dfac2b76f1a6151d585e3071fb10b1d23408e730
SiongSng <rrt467778@gmail.com>
2022-01-22 20:04:04 +0800

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# PRMTW Official Discord Bot RPMTW 官方Discord機器人
--- events/voiceStateUpdate.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (vd, v, log) => {
-  log.send(`**${v.member.user.tag}**(\`${v.member.id}\`) 離開 **${vd.channel.name}**(\`${vd.channel.id})\``);
-};
