2a43edea9aaeb53e697ad1767258869a68664492
SiongSng <rrt467778@gmail.com>
2022-01-23 09:24:38 +0800

    詐騙連結判斷

--- lib/events/message_create_event.dart
+++ lib/events/message_create_event.dart
@@ -1,5 +1,6 @@
 import 'package:nyxx/nyxx.dart';
 import 'package:rpmtw_discord_bot/events/base_event.dart';
+import 'package:rpmtw_discord_bot/utilities/data.dart';
 
 class MessageCreateEvent implements BaseEvent<IMessageReceivedEvent> {
   @override
@@ -10,27 +11,81 @@ class MessageCreateEvent implements BaseEvent<IMessageReceivedEvent> {
         r"(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$");
 
     if (urlRegex.hasMatch(messageContent)) {
+      /// 訊息內容包含網址
+
       List<RegExpMatch> matchList =
           urlRegex.allMatches(messageContent).toList();
 
       List<String> domainWhitelist = [
+        // DC 官方域名
         "discord.gift",
         "discord.gg",
         "discord.com",
         "discordapp.com",
         "discordapp.net",
-        "rpmtw.com",
-        "rpmtw.ga",
-        "google.com",
+
+        /// 社群域名
+        "discordresources.com",
+        "discord.wiki",
+
+        // Steam 官方域名
+        "steampowered.com",
+        "steamcommunity.com",
+
+        // 在 Alexa 名列前茅的 .gift 和 .gifts 域名
+        "crediter.gift",
+        "packedwithpurpose.gifts",
+        "123movies.gift",
+        "admiralwin.gift",
+        "gol.gift",
+        "newhome.gifts"
       ];
 
       for (RegExpMatch match in matchList) {
         String domain1 = match.group(2)!.split(".").last;
         String domain2 = match.group(3)!;
         String domain = "$domain1.$domain2";
-        print(domain);
-        if (!domainWhitelist.contains(domain)) {}
+
+        bool isWhitelisted = domainWhitelist.contains(domain);
+        bool isBlacklisted = phishingLinkList.contains(domain);
+        bool isUnknownSuspiciousLink = domain1.contains("disc") ||
+            domain1.contains("steam") ||
+            domain2.contains("gift");
+
+        bool phishing = !isWhitelisted && (isBlacklisted || isUnknownSuspiciousLink);
+
+        if (phishing) {
+          /// 符合詐騙連結條件
+          _onPhishing(event.message, ban: true);
+        }
+      }
+    } else if (phishingTermList.any((e) => messageContent.contains(e))) {
+      /// 詐騙關鍵字
+      _onPhishing(event.message, ban: false);
+    }
+  }
+
+  Future<void> _onPhishing(IMessage message, {required bool ban}) async {
+    final ReplyBuilder replyBuilder = ReplyBuilder.fromMessage(message);
+    IMessageAuthor author = message.author;
+    MessageBuilder messageBuilder = MessageBuilder.content(
+        "偵測到 <@${author.id}> 發送了詐騙訊息，因此已立即停權 <@${author.id}>，其他人請勿點選此詐騙訊息。");
+    messageBuilder.replyBuilder = replyBuilder;
+
+    await message.channel.sendMessage(messageBuilder);
+    await message.delete();
+
+    if (message.guild != null) {
+      IGuild guild = message.guild!.getFromCache()!;
+      String auditReason =
+          "違反RPMTW Discord伺服器規範第一條，不得以任何形式騷擾他人，散布不實詐騙訊息，如認為有誤判，請使用 Email 聯絡 rrt46777@gmail.com，並附上您的 Discord ID。";
+
+      if (ban) {
+        guild.ban(author, deleteMessageDays: 1, auditReason: auditReason);
+      } else {
+        guild.kick(author, auditReason: auditReason);
       }
     }
+    await logger.info("偵測到 <@${author.id}> 在 <#${message.channel.id}> 發送詐騙訊息");
   }
 }
--- lib/utilities/data.dart
+++ lib/utilities/data.dart
@@ -1,17 +1,35 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dotenv/dotenv.dart';
 import 'package:nyxx/nyxx.dart';
+import 'package:path/path.dart';
 import 'package:rpmtw_api_client/rpmtw_api_client.dart';
 import 'package:rpmtw_discord_bot/utilities/log.dart';
 
 Snowflake get rpmtwDiscordServerID => 815819580840607807.toSnowflake();
 Snowflake get logChannelID => 934595900528025640.toSnowflake();
+Snowflake get siongsngUserID => 645588343228334080.toSnowflake();
+
 late Logger _logger;
 Logger get logger => _logger;
+late List<String> phishingLinkList;
+late List<String> phishingTermList;
 
 class Data {
   static void init() {
     load();
     RPMTWApiClient.init();
+
+    String phishingLink =
+        File(join(Directory.current.path, "lib", "data", "phishing_link.txt"))
+            .readAsStringSync();
+    phishingLinkList = LineSplitter().convert(phishingLink);
+
+    String phishingTerms =
+        File(join(Directory.current.path, "lib", "data", "phishing_terms.txt"))
+            .readAsStringSync();
+    phishingTermList = LineSplitter().convert(phishingTerms);
   }
 
   static Future<void> initOnReady(INyxxWebsocket client) async {
