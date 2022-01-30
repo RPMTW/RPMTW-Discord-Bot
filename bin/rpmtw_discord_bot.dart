c3078728c01a274e41176f686a895f5d869aa180
SiongSng <rrt467778@gmail.com>
2022-01-30 14:07:46 +0800

    test

--- bin/rpmtw_discord_bot.dart
+++ bin/rpmtw_discord_bot.dart
@@ -24,7 +24,7 @@ void main(List<String> arguments) async {
   client.connect();
 
   /// Register all commands
-  Interactions.register(client);
+  // Interactions.register(client);
 
   /// Register all events
   Events.register(client);
